5dcb20649993b93e5572588f2393545f26e291b5
henriquenagy <nagy-martins@hotmail.com>
2024-03-08 13:43:56 -0300

    ok

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -82,6 +82,7 @@ console.log(fruitProcessor(2, 3))
 
 /* Write your code below. Good luck! 🙂 */
 
+/*
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3
 
 //test1
@@ -104,3 +105,14 @@ scoreDolphins = calcAverage(85, 54, 41)
 scoreKoalas = calcAverage(23, 34, 27)
 console.log(scoreDolphins, scoreKoalas)
 checkWinner(scoreDolphins, scoreKoalas)
+*/
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+}
+/* //OU FORMA ENXUTA PELO ARROW
+const calcTip = bill => (bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2)*/
+const bills = [125, 555, 44]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+console.log(bills, tips, totals)
