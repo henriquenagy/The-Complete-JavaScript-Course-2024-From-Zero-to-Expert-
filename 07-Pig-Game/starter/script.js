62cb4b7fb8215827422172e2d7c054e6e51ab1e1
henriquenagy <nagy-martins@hotmail.com>
2024-03-22 12:04:47 -0300

    New class

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+let rollDice = document.querySelector('.btn--roll');
+let diceNumberRoll = document.querySelector('#current--1');
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const diceImage = document.querySelector('.dice');
+
+score0.textContent = 0;
+score1.textContent = 0;
+diceImage.classList.add('hidden'); //Inseri a classe hidden em css, depois coloquei aqui
+
+rollDice.addEventListener('click', function () {
+  let number = Math.trunc(Math.random() * 6) + 1;
+  diceNumberRoll.textContent = number;
+});
