31abeb302acfeca130926775e901749d2fb2579d
henriquenagy <nagy-martins@hotmail.com>
2024-04-03 15:04:32 -0300

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -36,10 +36,64 @@ var numProducts = 10; //Com var funciona depois do if OK
 function deleteShoppingCart() {
   console.log('All products deleted!');}*/
 
-//Se digitar window no console navegador aparece todos os js usados no código. Porém só vair aparecer os que tiverem sido criados com VAR. pois somente esses vão ter criado a property on window object
+/*  02/04/24 ----------------------------------------
+  //Se digitar window no console navegador aparece todos os js usados no código. Porém só vair aparecer os que tiverem sido criados com VAR. pois somente esses vão ter criado a property on window object
 var x = 1; //true
 let y = 2; //false
 const z = 3; //false
 console.log(x === window.x);
 console.log(y === window.y);
 console.log(z === window.z);
+*/
+
+//----------------------------------- 03/04/2024  USO DO THIS
+/* ERRO AO USAR ARROW FUNCTION COM O THIS
+var firstName = 'Matilda'; //Aqui vai aparecer dentro do greet(), ERRO FATAL
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+  //arrow function ñ dá para usar o this, pois ele pega o global var
+  greet: () => {
+    console.log(this); //ERROR vai pegar o global object, e não o de dentro
+    console.log(`Hey ${this.firstName}`); //ERRO FATAL vai pegar a matilda global do Var
+  },
+};
+jonas.greet();*/
+
+/*
+// Uso do this em uma função normal dentro de outra função normal
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(2037 - this.year);
+    //O pulo do gato aqui é criar uma varíavel que seja igual ao this, ai funciona
+    const self = this; // self or that
+    const isMillenial = function () {
+      console.log(self);
+      console.log(self.year >= 1981 && self.year <= 1996);
+    };
+    isMillenial();
+  },
+};
+jonas.calcAge();*/
+
+// Uso do this em uma arrow function dentro de outra função normal
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(2037 - this.year);
+    // Aqui no arrow dentro de função dá certo,ñ precisa criar outra variável que represente o this, já pega direto
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+};
+jonas.calcAge();
