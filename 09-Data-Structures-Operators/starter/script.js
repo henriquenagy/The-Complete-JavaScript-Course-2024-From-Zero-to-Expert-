901382145c51dc30f28585d23cb6a872e20567e7
henriquenagy <nagy-martins@hotmail.com>
2024-04-05 10:30:05 -0300

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,43 @@
 'use strict';
+//-------------------------------------------05-04-2024
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+//Pegar dois dados do array do objeto acima, o 1o e o 3o, deixa o 2o vazio
+let [first, , third] = restaurant.categories;
+console.log(first, third);
+
+//pegar todos numeros de um array
+const arrays = [2, 3, 4];
+const [x, y, z] = arrays;
+console.log(x, y, z);
+
+//switching variables
+[first, third] = [third, first];
+console.log(first, third);
+
+//Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//Nested elements destructuring
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -27,3 +65,4 @@ const restaurant = {
     },
   },
 };
+*/
