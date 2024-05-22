2badbeb402e369b00f2cb78b8a25ff0c771247dd
henriquenagy <nagy-martins@hotmail.com>
2024-05-22 15:14:35 -0300

    weds

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,43 @@
 'use strict'
+/*//132. Functions Accepting Callback Functions
+const oneWord = function (qqerText) {
+  return qqerText.replace(/ /g, '').toLowerCase()
+}
+const upperFirstWord = function (qqerText) {
+  const [prime, ...restin] = qqerText.split(' ')
+  return [prime.toUpperCase(), ...restin].join(' ')
+}
+//Higher-order function
+const transformer = function (qqerText, fn) {
+  console.log(`Original string: ${qqerText}`)
+  console.log(`Transformed string: ${fn(qqerText)}`)
+  console.log(`Created by the function: ${fn.name}`) //name aqui é para saber mesmo só o nome da função criada
+}
+transformer('Javascript is the best', upperFirstWord)
+//Original string: Javascript is the best
+//Transformed string: JAVASCRIPT is the best
+//Created by the function: upperFirstWord
+transformer('Javascript is the best', oneWord)
+//Original string: Javascript is the best
+//Transformed string: javascriptisthebest
+//Created by the function: oneWord
+
+const high5 = function () {
+  console.log('🖐')
+}
+document.body.addEventListener('click', high5)
+*/
+
+//133. Function returning functions  22/05/24
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`)
+  }
+}
+const greeterHey = greet('Hey') //Variável que vira uma function
+greeterHey('Henrique') // Chamando a variável que é function
+greeterHey('Daniele')
+greet('Hello')('Najão') //Chamando a função que tem outra dentro, mas sem mencionar essa de dentro.
+
+const greet2 = greeting => name => console.log(`${greeting} ${name}`)
+greet2('Hello')('Mr. nagys')
--- RESOLUÇÕES EXERCÍCIOS DO SITE/001. Apenas testes para word.js
+++ RESOLUÇÕES EXERCÍCIOS DO SITE/001. Apenas testes para word.js
@@ -1,9 +1,6 @@
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30'
-//Arrow function para usar dentro do const output
-const getcode = str => str.slice(0, 3).toUpperCase() //Só para encurtar o code dentro do output
-
-for (let itens of flights.split('+')) {
-  const [type, from, to, time] = itens.split(';')
+const calcAge2 = function (birthYear) {
+  // uma variável guarda o resultado da function
+  return 2024 - birthYear
 }
-console.log(type, from, to, time)
+const age2 = calcAge2(1990) // A variável chamadora vem após a function, caso contrário ñ funciona
+console.log(age2) // 34
