7058e0f40aae38f53a8a297f08bbe2e5c79c7e34
henriquenagy <nagy-martins@hotmail.com>
2024-06-06 14:29:47 -0300

    thursday

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,4 +1,3 @@
-'use strict'
 /*//132. Functions Accepting Callback Functions
 const oneWord = function (qqerText) {
   return qqerText.replace(/ /g, '').toLowerCase()
@@ -189,8 +188,8 @@ booker() //1 passengers
 booker() //2 passengers
 booker() //3 passengers
 console.dir(booker) //Checa tudo sobre a function, incluindo o closure*/
-//Closure aula 2
-/*//EXAMPLE 1
+/*//Closure aula 2
+//EXAMPLE 1
 let f //Global scope, ai todas podem acessar
 const g = function () {
   const a = 3
@@ -213,7 +212,7 @@ console.dir(f) //Closure(g) a = 3
 //Re-assigning f function
 h()
 f()
-console.dir(f) //Closure(h) b = 4*/
+console.dir(f) //Closure(h) b = 4
 //EXAMPLE 2
 const boardPassengers = function (n, wait) {
   //const perGroup = n / 3
@@ -224,4 +223,21 @@ const boardPassengers = function (n, wait) {
   console.log(`Will start boarding in ${wait} seconds`)
 }
 const perGroup = 1000
-boardPassengers(180, 3)
+boardPassengers(180, 3)*/
+//140. Coding Challenge #2 06/06/2024 Quinta - Coding Challenge #2
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+;(function () {
+  const textu = document.querySelector('h1')
+  textu.style.color = 'red'
+  window.document.body.addEventListener('click', function () {
+    textu.style.color = 'blue'
+  })
+})()
+
+//Se meu function do click no body estivesse fora, não daria pra usar a const textu, teria que criar outra.
