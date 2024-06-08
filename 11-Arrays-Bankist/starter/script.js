a99b311f70af6943b13291470e9d45b044abe3d2
henriquenagy <nagy-martins@hotmail.com>
2024-06-08 15:40:06 -0300

    sabd

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -9,68 +9,98 @@ const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
-  pin: 1111,
-};
+  pin: 1111
+}
 
 const account2 = {
   owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
-  pin: 2222,
-};
+  pin: 2222
+}
 
 const account3 = {
   owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
-  pin: 3333,
-};
+  pin: 3333
+}
 
 const account4 = {
   owner: 'Sarah Smith',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
-  pin: 4444,
-};
+  pin: 4444
+}
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2, account3, account4]
 
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+const labelWelcome = document.querySelector('.welcome')
+const labelDate = document.querySelector('.date')
+const labelBalance = document.querySelector('.balance__value')
+const labelSumIn = document.querySelector('.summary__value--in')
+const labelSumOut = document.querySelector('.summary__value--out')
+const labelSumInterest = document.querySelector('.summary__value--interest')
+const labelTimer = document.querySelector('.timer')
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+const containerApp = document.querySelector('.app')
+const containerMovements = document.querySelector('.movements')
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+const btnLogin = document.querySelector('.login__btn')
+const btnTransfer = document.querySelector('.form__btn--transfer')
+const btnLoan = document.querySelector('.form__btn--loan')
+const btnClose = document.querySelector('.form__btn--close')
+const btnSort = document.querySelector('.btn--sort')
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+const inputLoginUsername = document.querySelector('.login__input--user')
+const inputLoginPin = document.querySelector('.login__input--pin')
+const inputTransferTo = document.querySelector('.form__input--to')
+const inputTransferAmount = document.querySelector('.form__input--amount')
+const inputLoanAmount = document.querySelector('.form__input--loan-amount')
+const inputCloseUsername = document.querySelector('.form__input--user')
+const inputClosePin = document.querySelector('.form__input--pin')
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
+//08/06/2024
+/*//As duas maneiras abaixo, uma é usando for of e outra usando foreach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
+for (const movement of movements) {
+  if (movement > 0) console.log(`You deposited ${movement}`)
+    else console.log(`You withdrew ${Math.abs(movement)}`)
+}
+console.log('-----------Usando com o Foreach')
+movements.forEach(function (insideNumbers) {
+  if (insideNumbers > 0) console.log(`You've deposited ${insideNumbers}`)
+    else console.log(`You withdrew ${Math.abs(insideNumbers)}`)
+})
+//Agora as duas abaixo: a 1ª é usando for of com duas variaveis e entries, e a 2ª é uma espécie de entries com function
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) console.log(`You deposited ${movement}`)
+    else console.log(`You withdrew ${Math.abs(movement)}`)
+}
+console.log('-----------Usando com o Foreach, function e 3 itens dentro')
+movements.forEach(function (currentElement, currentIndex, entireArray) {
+  if (currentElement > 0) console.log(`You deposited ${currentElement}`)
+  else console.log(`You withdrew ${Math.abs(currentElement)}`)
+})*/
+
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+  ['GBP', 'Pound sterling']
+])
+//Map()
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`)
+})
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+//set() O funcionamento para ele é diferente dos outros, não tem como definir uma key pois não tem. ai usa o underline pois ele só tem um valor único, mas não pode repetir nome pq dá problema no console
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR', 'USD'])
+console.log(currenciesUnique)
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`)
+})
