580ac620eb1b92b195825908d39260668cfe1254
henriquenagy <nagy-martins@hotmail.com>
2024-06-19 14:11:04 -0300

    weds

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,29 +1,4 @@
 'use strict'
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111
-}
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222
-}
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333
-}
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444
-}
-const accounts = [account1, account2, account3, account4]
 const labelWelcome = document.querySelector('.welcome')
 const labelDate = document.querySelector('.date')
 const labelBalance = document.querySelector('.balance__value')
@@ -31,16 +6,13 @@ const labelSumIn = document.querySelector('.summary__value--in')
 const labelSumOut = document.querySelector('.summary__value--out')
 const labelSumInterest = document.querySelector('.summary__value--interest')
 const labelTimer = document.querySelector('.timer')
-
 const containerApp = document.querySelector('.app')
 const containerMovements = document.querySelector('.movements')
-
 const btnLogin = document.querySelector('.login__btn')
 const btnTransfer = document.querySelector('.form__btn--transfer')
 const btnLoan = document.querySelector('.form__btn--loan')
 const btnClose = document.querySelector('.form__btn--close')
 const btnSort = document.querySelector('.btn--sort')
-
 const inputLoginUsername = document.querySelector('.login__input--user')
 const inputLoginPin = document.querySelector('.login__input--pin')
 const inputTransferTo = document.querySelector('.form__input--to')
@@ -48,53 +20,113 @@ const inputTransferAmount = document.querySelector('.form__input--amount')
 const inputLoanAmount = document.querySelector('.form__input--loan-amount')
 const inputCloseUsername = document.querySelector('.form__input--user')
 const inputClosePin = document.querySelector('.form__input--pin')
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
+
+const account1 = {
+ owner: 'Jonas Schmedtmann',
+ movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+ interestRate: 1.2, // %
+ pin: 1111
+}
+const account2 = {
+ owner: 'Jessica Davis',
+ movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+ interestRate: 1.5,
+ pin: 2222
+}
+const account3 = {
+ owner: 'Steven Thomas Williams',
+ movements: [200, -200, 340, -300, -20, 50, 400, -460],
+ interestRate: 0.7,
+ pin: 3333
+}
+const account4 = {
+ owner: 'Henrique Nagy',
+ movements: [49930, 100099, 709990, 5550, 9580, 65000, 89555],
+ interestRate: 5,
+ pin: 6969
+}
+const accounts = [account1, account2, account3, account4]
+
 //Insert the current balance on the page
 const currentBalancePage = function (somars) {
-  const results = somars.reduce((acc, mov) => acc + mov, 0)
-  labelBalance.textContent = `${results} €`
+ const results = somars.reduce((acc, mov) => acc + mov, 0)
+ labelBalance.textContent = `${results} €`
 }
-currentBalancePage(account1.movements)
 //Insert all account movements (deposit & withdraw)
 const displayMovements = function (movements) {
-  containerMovements.innerHTML = '' //Remove the old initial data showing in the page
-  movements.forEach(function (mov, i) {
-    const moneyINorOut = mov > 0 ? 'deposit' : 'withdrawal'
-    const html = `
+ containerMovements.innerHTML = '' //Remove the old initial data showing in the page
+ movements.forEach(function (mov, i) {
+  const moneyINorOut = mov > 0 ? 'deposit' : 'withdrawal'
+  const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${moneyINorOut}">${
-      i + 1
-    } ${moneyINorOut}</div>
+   i + 1
+  } ${moneyINorOut}</div>
     <div class="movements__value">${mov} €</div>
   </div>'`
-    containerMovements.insertAdjacentHTML('afterbegin', html)
-  })
+  containerMovements.insertAdjacentHTML('afterbegin', html)
+ })
 }
-displayMovements(account1.movements)
-//Money in
-const calcDisplaySummary = function (moneyBalance) {
-  const incomes = moneyBalance
-    .filter(mov => mov > 0)
-    .reduce((acc, mov) => acc + mov, 0)
-  labelSumIn.textContent = `${incomes}€`
-  //money out
-  const outcome = moneyBalance
-    .filter(mov => mov < 0)
-    .reduce((acc, mov) => acc + mov, 0)
-  labelSumOut.textContent = `${Math.abs(outcome)}€`
-  //Ganhos de dinheiro guardado, o 2º filter é para tirar valores menor que um da lista
-  const interest = moneyBalance
-    .filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
-    .filter((int, i, arr) => {
-      console.log(arr)
-      return int >= 1
-    })
-    .reduce((acc, int) => acc + int, 0)
-  labelSumInterest.textContent = `${interest}€`
-}
-calcDisplaySummary(account1.movements)
 
+//Money in, out and gains
+const calcDisplaySummary = function (acc) {
+ const incomes = acc.movements
+  .filter(mov => mov > 0)
+  .reduce((acc, mov) => acc + mov, 0)
+ labelSumIn.textContent = `${incomes}€`
+ //money out
+ const outcome = acc.movements
+  .filter(mov => mov < 0)
+  .reduce((acc, mov) => acc + mov, 0)
+ labelSumOut.textContent = `${Math.abs(outcome)}€`
+ //Ganhos de dinheiro guardado, o 2º filter é para tirar valores menor que um da lista
+ const interest = acc.movements
+  .filter(mov => mov > 0)
+  .map(deposit => (deposit * acc.interestRate) / 100)
+  .filter(int => int >= 1)
+  .reduce((acc, int) => acc + int, 0)
+ labelSumInterest.textContent = `${Math.abs(interest)}€`
+}
+
+//Criando usuários de acesso
+const createUsernames = function (accs) {
+ accs.forEach(function (acc) {
+  acc.username = acc.owner
+   .toLowerCase()
+   .split(' ')
+   .map(name => name[0])
+   .join('')
+ })
+}
+createUsernames(accounts)
+
+//Para acesso a tela do banco
+let currentAccount
+btnLogin.addEventListener('click', function (e) {
+ e.preventDefault() //para de dar reload quando clicar no botão
+
+ currentAccount = accounts.find(
+  acc => acc.username === inputLoginUsername.value
+ ) //Checar usuário atual para acesso
+ console.log(currentAccount)
+
+ //if (currentAccount && currentAccount.pin === Number(inputLoginPin.value)) OU o método abaixo
+ if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  inputLoginPin.value = inputLoginUsername.value = ''
+  inputLoginPin.blur()
+  //Display UI and message
+  labelWelcome.textContent = `Welcome ${currentAccount.owner.split(' ')[0]}`
+  containerApp.style.opacity = 100
+  //Display Balance
+  currentBalancePage(currentAccount.movements)
+  //Display Movements
+  displayMovements(currentAccount.movements)
+  //Display Summary
+  calcDisplaySummary(currentAccount)
+ }
+})
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
 /*//13/06 Getting the 1st name letter
 displayMovements(account1.movements)
 const user = 'Henrique Nagy Martins'
--- RESOLUÇÕES EXERCÍCIOS DO SITE/001. Apenas testes para word.js
+++ RESOLUÇÕES EXERCÍCIOS DO SITE/001. Apenas testes para word.js
@@ -1,12 +1,1 @@
-/* Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
-TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
-TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]*/
 
-const avg1 = [5, 2, 4, 1, 15, 8, 3]
-const avg2 = [16, 6, 10, 5, 6, 1, 4]
-const calcAverageHumanAge3 = avg1.map(age =>
-  age <= 2 ? 2 * age : 16 + age * 4
-)
-const adults = calcAverageHumanAge3.filter(age => age >= 18)
-const average = adults.reduce((acc, age, i, arr) => acc + age / arr.length, 0)
-console.log(`${calcAverageHumanAge3}\n${adults}\n${average}`)
