eb2cca1269a2a74b927f12b3a7839fddc8e1b0be
henriquenagy <nagy-martins@hotmail.com>
2024-08-05 23:07:26 -0300

    mondays

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -161,15 +161,6 @@ currentAccount = account1
 updateUI(currentAccount)
 containerApp.style.opacity = 100
 
-//Get & show only day, month and year day/month/year
-const now = new Date()
-const day = `${now.getDate()}`.padStart(2, 0)
-const month = `${now.getMonth() + 1}`.padStart(2, 0)
-const year = now.getFullYear()
-const hour = now.getHours()
-const min = now.getMinutes()
-labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`
-
 btnLogin.addEventListener('click', function (e) {
  // Prevent form from submitting
  e.preventDefault()
@@ -182,12 +173,22 @@ btnLogin.addEventListener('click', function (e) {
   labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`
   containerApp.style.opacity = 100
 
+  //Get & show only day, month and year day/month/year
+  const now = new Date()
+  const day = `${now.getDate()}`.padStart(2, 0)
+  const month = `${now.getMonth() + 1}`.padStart(2, 0)
+  const year = now.getFullYear()
+  const hour = `${now.getHours()}`.padStart(2, 0)
+  const min = `${now.getMinutes()}`.padStart(2, 0)
+  labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`
+
   // Clear input fields
   inputLoginUsername.value = inputLoginPin.value = ''
-  inputLoginPin.blur()
+  inputLoginPin
+   .blur()
 
-  // Update UI
-  updateUI(currentAccount)
+   // Update UI
+   .updateUI(currentAccount)
  }
 })
 
@@ -207,6 +208,10 @@ btnTransfer.addEventListener('click', function (e) {
   currentAccount.movements.push(-amount)
   receiverAcc.movements.push(amount)
 
+  //Add transfer date
+  currentAccount.movementsDates.push(new Date().toISOString())
+  receiverAcc.movementsDates.push(new Date().toISOString())
+
   // Update UI
   updateUI(currentAccount)
  }
@@ -220,6 +225,9 @@ btnLoan.addEventListener('click', function (e) {
  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
   // Add movement
   currentAccount.movements.push(amount)
+
+  //Add loan date
+  currentAccount.movementsDates.push(new Date().toISOString()) //"2024-08-06T02:02:58.277Z"
 
   // Update UI
   updateUI(currentAccount)
