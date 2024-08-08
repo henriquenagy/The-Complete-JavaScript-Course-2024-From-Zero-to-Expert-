adad12d8b66aa07d8a33c8a83f1e62a178bd1d23
henriquenagy <nagy-martins@hotmail.com>
2024-08-08 17:32:43 -0300

    quintou

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -95,6 +95,13 @@ const formatMovementDate = function (date, locale) {
  return new Intl.DateTimeFormat(locale).format(date)
 }
 
+const formatCur = function (value, locale, currency) {
+ return new Intl.NumberFormat(locale, {
+  style: 'currency',
+  currency: currency
+ }).format(value)
+}
+
 const displayMovements = function (acc, sort = false) {
  containerMovements.innerHTML = ''
 
@@ -106,11 +113,13 @@ const displayMovements = function (acc, sort = false) {
   const date = new Date(acc.movementsDates[i])
   const displayDate = formatMovementDate(date, acc.locale)
 
+  const formattedMov = formatCur(mov, acc.locale, acc.currency)
+
   const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
         <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${mov.toFixed(2)}€</div>
+        <div class="movements__value">${formattedMov}</div>
       </div>
     `
 
@@ -120,15 +129,15 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0)
- labelBalance.textContent = `${acc.balance.toFixed(2)}€`
+ labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency)
 }
 
 const calcDisplaySummary = function (acc) {
  const incomes = acc.movements.filter(mov => mov > 0).reduce((acc, mov) => acc + mov, 0)
- labelSumIn.textContent = `${incomes.toFixed(2)}€`
+ labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency)
 
  const out = acc.movements.filter(mov => mov < 0).reduce((acc, mov) => acc + mov, 0)
- labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`
+ labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency)
 
  const interest = acc.movements
   .filter(mov => mov > 0)
@@ -138,7 +147,7 @@ const calcDisplaySummary = function (acc) {
    return int >= 1
   })
   .reduce((acc, int) => acc + int, 0)
- labelSumInterest.textContent = `${interest.toFixed(2)}€`
+ labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency)
 }
 
 const createUsernames = function (accs) {
@@ -245,14 +254,17 @@ btnLoan.addEventListener('click', function (e) {
  const amount = Math.floor(inputLoanAmount.value)
 
  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-  // Add movement
-  currentAccount.movements.push(amount)
+  //atraso na solicitação loan
+  setTimeout(function () {
+   // Add movement
+   currentAccount.movements.push(amount)
 
-  //Add loan date
-  currentAccount.movementsDates.push(new Date().toISOString()) //"2024-08-06T02:02:58.277Z"
+   //Add loan date
+   currentAccount.movementsDates.push(new Date().toISOString()) //"2024-08-06T02:02:58.277Z"
 
-  // Update UI
-  updateUI(currentAccount)
+   // Update UI
+   updateUI(currentAccount)
+  }, 2500)
  }
  inputLoanAmount.value = ''
 })
@@ -442,15 +454,29 @@ const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 
 const days1 = calcDaysPassed(new Date(2025, 3, 14), new Date(2025, 3, 4))
 console.log(days1) // 10 - Math abs deixa data positivo, mesmo sendo 10 dias menos/antes*/
 
-//ss
+/*// 08/08 trabalhando com njumeros 179 
 const num = 434322342
-
 const options = {
  style: 'currency',
  unit: 'celsius',
  currency: 'BRL'
 }
-
 console.log('Trump:     ', new Intl.NumberFormat('en-US', options).format(num))
 console.log('Braza:     ', new Intl.NumberFormat('pt-BR', options).format(num))
-console.log('Germany:     ', new Intl.NumberFormat('de-DE', options).format(num))
+console.log('Germany:     ', new Intl.NumberFormat('de-DE', options).format(num))*/
+
+/* //08/08/24 181. Timers Set timeout and setInterval
+const ingredients = ['olives', 'spinach']
+const pizzaTimer = setTimeout(
+ (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+ 3000,
+ ...ingredients
+)
+//Limpar timer se tiver spinach
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer)*/
+setInterval(function () {
+ const now = new Date()
+ console.log(now)
+}, 12000)
+
+//sdS
