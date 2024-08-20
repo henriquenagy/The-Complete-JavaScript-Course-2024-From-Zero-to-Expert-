000cec5651b4d755c00985081ce443698637673f
henriquenagy <nagy-martins@hotmail.com>
2024-08-20 19:13:43 -0300

    terças

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -3,6 +3,10 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.btn--close-modal')
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal')
+const btnScrollto = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+const navItens = document.querySelectorAll('.nav__link')
+const outsideUlItens = document.querySelector('.nav__links')
 
 const openModal = function (e) {
  e.preventDefault()
@@ -16,7 +20,6 @@ const closeModal = function () {
 }
 
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
-
 btnCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
 
@@ -26,7 +29,23 @@ document.addEventListener('keydown', function (e) {
  }
 })
 
-//Aprendizados da aula 189 scroll dia 16/08
+//Button scrolling (Ver aula do dia 16/08)
+btnScrollto.addEventListener('click', function (e) {
+ section1.scrollIntoView({ behavior: 'smooth' })
+})
+
+//Método avançado para o page navigation (o simples ver o dia 20/08)
+outsideUlItens.addEventListener('click', function (e) {
+ e.preventDefault()
+ console.log(e.target) //Mostra onde foi o clique, só pra vc saber
+
+ if (e.target.classList.contains('nav__link')) {
+  const id = e.target.getAttribute('href')
+  document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+ }
+})
+
+/* ///////////////  Aprendizados da aula 189 scroll dia 16/08  /////////////////////
 const btnScrollto = document.querySelector('.btn--scroll-to')
 const section1 = document.querySelector('#section--1')
 
@@ -39,53 +58,53 @@ btnScrollto.addEventListener('click', function (e) {
  console.log(`Current scroll X: ${window.scrollX} and Y: ${window.scrollY}`) //Current scroll X: 0 and Y: 394 > A medida que move a tela altera os valores. De inicio fica tudo zero
  console.log(
   `Height viewport: ${document.documentElement.clientHeight} and width viewport: ${document.documentElement.clientWidth}`
- ) //Height viewport: 572 and width viewport: 904
-
- /* //Agora sim bora scrolling the page
- window.scrollTo(s1coords.left + window.scrollX, s1coords.top + window.scrollY) // Método base
- window.scrollTo({
-  left: s1coords.left + window.scrollX,
-  top: s1coords.top + window.scrollY,
-  behavior: 'smooth'
- }) //Pra usar o behavior precisa especificar o left e top
-*/
- //Maneira mais moderna do jonas
- section1.scrollIntoView({ behavior: 'smooth' }) // Só pega em browser mais recentes
-})
+  ) //Height viewport: 572 and width viewport: 904
+  
+  //Agora sim bora scrolling the page
+  window.scrollTo(s1coords.left + window.scrollX, s1coords.top + window.scrollY) // Método base
+  window.scrollTo({
+   left: s1coords.left + window.scrollX,
+   top: s1coords.top + window.scrollY,
+   behavior: 'smooth'
+   }) //Pra usar o behavior precisa especificar o left e top
+   
+   //Maneira mais moderna do jonas
+   section1.scrollIntoView({ behavior: 'smooth' }) // Só pega em browser mais recentes
+   })*/
 
 /*////////////////////////// aula 187 dia 14/08/24 //////////////////////////////////
-//console.log(document.documentElement) //Pega toda a estrutura html
-//console.log(document.head) // Só a parte do head
-//console.log(document.body) //E esse todo o body
-
-//Nodelist - se eu atualizar algum item da pasta (remover, por ex) ele não atualiza sozinho, se mantem na forma original como de inicio
-const allSections = document.querySelectorAll('.section')
-//console.log(allSections) // NodeList(4) [section#section--1.section, section#section--2.section, section#section--3.section, section.section.section--sign-up]
-
-//HTMLcollection - esse é o inverso de node, atualiza sozinho após qqer ação (remover inserir)
-document.getElementById('section--1')
-const allButtons = document.getElementsByTagName('button')
-//console.log(allButtons) // HTMLCollection(9) (mostra todos os botões e suas classes)
-//console.log(document.getElementsByClassName('btn')) //HTMLCollection(5) -  Botões com a classe somente
-
-//Creating and inserting elements with IS inside HTML
-const header = document.querySelector('.header')
-const message = document.createElement('div')
-message.classList.add('cookie-message')
-//message.textContent = 'We use cookies for improved functionality and analytics.'
-message.innerHTML =
- 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'
-
-//header.prepend(message) // Ou usa esse(vai pra cima/antes)
-header.append(message) //Ou usa esse (baixo/depois)
-//header.append(message.cloneNode(true)) //Esse aqui vc pode combinar com os anteriores, ai aparece nas duas posições. Os anteriores juntos não funciona.
-
-//header.before(message) //Outra forma, ao invés de usar prepend e append
-//header.after(message)
-
-// Delete inserted div element
-document.querySelector('.btn--close-cookie').addEventListener('click', function () {
- message.parentElement.removeChild(message)
+  //console.log(document.documentElement) //Pega toda a estrutura html
+  //console.log(document.head) // Só a parte do head
+  //console.log(document.body) //E esse todo o body
+  
+  //Nodelist - se eu atualizar algum item da pasta (remover, por ex) ele não atualiza sozinho, se mantem na forma original como de inicio
+  const allSections = document.querySelectorAll('.section')
+  //console.log(allSections) // NodeList(4) [section#section--1.section, section#section--2.section, section#section--3.section, section.section.section--sign-up]
+  
+  //HTMLcollection - esse é o inverso de node, atualiza sozinho após qqer ação (remover inserir)
+  document.getElementById('section--1')
+  const allButtons = document.getElementsByTagName('button')
+  //console.log(allButtons) // HTMLCollection(9) (mostra todos os botões e suas classes)
+  //console.log(document.getElementsByClassName('btn')) //HTMLCollection(5) -  Botões com a classe somente
+  
+  //Creating and inserting elements with IS inside HTML
+  const header = document.querySelector('.header')
+  const message = document.createElement('div')
+  message.classList.add('cookie-message')
+  //message.textContent = 'We use cookies for improved functionality and analytics.'
+  message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'
+  
+  //header.prepend(message) // Ou usa esse(vai pra cima/antes)
+  header.append(message) //Ou usa esse (baixo/depois)
+  //header.append(message.cloneNode(true)) //Esse aqui vc pode combinar com os anteriores, ai aparece nas duas posições. Os anteriores juntos não funciona.
+  
+  //header.before(message) //Outra forma, ao invés de usar prepend e append
+  //header.after(message)
+  
+  // Delete inserted div element
+  document.querySelector('.btn--close-cookie').addEventListener('click', function () {
+   message.parentElement.removeChild(message)
 })
 
 //Styles [15/08/24 aula 188]
@@ -112,6 +131,7 @@ console.log(logo.className) //nav__logo
 logo.alt = 'Trocando alt da logo' //<img src="img/logo.png" alt="Trocando alt da logo" class="nav__logo" id="logo">
 
 //Non-standard - designer não é padrão html, eu que criei isso lá
+
 console.log(logo.designer) //não pega pois não é padrão
 console.log(logo.getAttribute('designer')) //najao - esse pega
 logo.setAttribute('company', 'bankist') // Insere um atributo não padrão á logo, no código html
@@ -170,7 +190,7 @@ const alertH1 = function () {
 h1.addEventListener('mouseenter', alertH1)
 setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 10000)*/
 
-//////////////////////////// aula 192 dia 19/08/24 //////////////////////////////////
+/* //////////////////////////// aula 192 dia 19/08/24 //////////////////////////////////
 // Event Propagation in Practice
 const GeradorAleatory = () => Math.trunc(Math.random() * 255) + 1
 const randColor = () => {
@@ -198,4 +218,14 @@ document.querySelector('.nav').addEventListener(
   console.log('NAV', e.target, e.currentTarget)
  } //,true
  //> se por true aqui, o primeiro item a aparecem como clicado é o NAV antes dos outros 2
-)
+)*/
+
+/*//////////////////////////// Page navigation (20/08/24) //////////////////////////////////
+navItens.forEach(function (el) {
+ el.addEventListener('click', function (e) {
+  e.preventDefault()
+  const id = this.getAttribute('href')
+  console.log(id)
+  document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+ })
+})*/
