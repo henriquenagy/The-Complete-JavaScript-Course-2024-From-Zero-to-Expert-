f9039135eda05a70e804c28aa31f5f54653cdf82
henriquenagy <nagy-martins@hotmail.com>
2024-09-02 14:33:10 -0300

    segundas

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
-'use strict';
+'use strict'
+class Carro {
+ constructor(marca, modelo) {
+  this.marca = marca
+  this.modelo = modelo
+ }
+ ligar() {
+  console.log(`${this.marca} ${this.modelo} ligado!`)
+ }
+}
+const meuCarro = new Carro('Toyota', 'Corolla')
+meuCarro.ligar() // 'Toyota Corolla ligado!'
+
+class CarroEletrico extends Carro {
+ constructor(marca, modelo, bateria) {
+  super(marca, modelo) // Chama o construtor da classe `Carro`
+  this.bateria = bateria
+ }
+ recarregar() {
+  console.log(`${this.marca} ${this.modelo} com ${this.bateria}% de bateria está recarregando.`)
+ }
+}
+
+const carroNovo = new CarroEletrico('Tesla', 'Model S', 80)
+carroNovo.ligar() // 'Tesla Model S ligado!'
+carroNovo.recarregar() // 'Tesla Model S com 80% de bateria está recarregando.'
