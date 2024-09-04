9f5b204f497a6c730c856c9d4bba85a64d4f3cfe
henriquenagy <nagy-martins@hotmail.com>
2024-09-04 11:39:12 -0300

    quarta

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,4 +1,45 @@
 'use strict'
+//Constructor Function Dissecado
+const construFuncs = function () {
+ const Person = function (firstName, birthYear) {
+  this.firstName = firstName
+  this.birthYear = birthYear
+ }
+ const nagys = new Person('Nagys', 1994) // Object __proto__
+ const dirclei = new Person('Dirclei', 1975) // Object __proto__
+
+ //Usa as infos do constructor, e passa aos objects somente quando chamado
+ Person.prototype.calcAge = function () {
+  console.log(2024 - this.birthYear)
+ }
+
+ nagys.calcAge() // 30
+ console.log(nagys) // Person {firstName: 'Nagys', birthYear: 1994} - Ñ aparece calcage
+
+ console.log(nagys.__proto__ === Person.prototype) // true
+ console.log(Person.prototype.isPrototypeOf(nagys)) // true
+ console.log(Person.prototype.isPrototypeOf(Person)) // false
+
+ Person.prototype.species = 'Homo Sapiens'
+ console.log(nagys, dirclei) // species: "Homo Sapiens" - Aparece em prototype
+ console.log(nagys.species, dirclei.species) // Já pega direto - Homo Sapiens Homo Sapiens
+ console.log(nagys.hasOwnProperty('firstName')) // true
+ console.log(nagys.hasOwnProperty('species')) // false
+
+ console.log(nagys.__proto__) // {species: 'Homo Sapiens', calcAge: ƒ}
+ console.log(nagys.__proto__.__proto__) // {__defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, __lookupSetter__: ƒ, …}
+ console.log(nagys.__proto__.__proto__.__proto__) //Null
+
+ console.log(Person.prototype.constructor) //ƒ (firstName, birthYear) {this.firstName = firstName;  this.birthYear = birthYear; } - mostra o constructor completo
+ console.dir(Person.prototype.constructor) //ƒ Person(firstName, birthYear) - mostra só a função
+
+ const arr = [3, 7, 9, 4] // é o mesmo que new Array, pois já herda todo o prototype veja o link MDN abaixo array filter
+ console.log(arr.__proto__) //[at: ƒ, concat: ƒ, copyWithin: ƒ, fill: ƒ, find: ƒ, …] tudo oq podemos usar em array (filter, fill, find, flatmap) já puxa tudo de prototype padrão
+ console.log(arr.__proto__ === Array.prototype) //true
+ console.log(arr.__proto__.__proto__) //{__defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, __lookupSetter__: ƒ, …}
+}
+construFuncs()
+/* /////////////////////// Dia 03/09 aula 209 e 210 ///////////////////////////
 const Person = function (firstName, birthYear) {
  //console.log(this) // sem nada de dados, retorna só Person
 
@@ -11,10 +52,10 @@ const Person = function (firstName, birthYear) {
 }
 const nagys = new Person('Nagys', 1994)
 const dirclei = new Person('Dirclei', 1975)
-/*1. New {} is created, ou seja, criou um empty object novo
+1. New {} is created, ou seja, criou um empty object novo
 2. Function Person is called, e o this passa a ser o objeto novo {}
 3. {} linked to a prototype
-4. function automatically return {}*/
+4. function automatically return {}
 console.log(nagys, dirclei) // Person {firstName: 'Nagys', birthYear: 1994} etc...
 
 const nonEkiziste = 'Notienes' //Sem os dados retorna false
@@ -43,14 +84,16 @@ console.log(Person.prototype.isPrototypeOf(Person)) // false
 
 Person.prototype.species = 'Homo Sapiens'
 console.log(nagys, dirclei) // Na primeira parte do console não aparece. Vc tem que ir abrindo as setas/opções e ver em prototype que aparece o novo item species adicionado
-/* Person {firstName: 'Nagys', birthYear: 1994}
+Person {firstName: 'Nagys', birthYear: 1994}
 birthYear: 1994
 firstName: "Nagys"
 [[Prototype]] : Object
 calcAge : ƒ ()
-species: "Homo Sapiens"*/
+species: "Homo Sapiens"
 console.log(nagys.species, dirclei.species) // Já pega direto - Homo Sapiens Homo Sapiens
 
 //Porém vc pode notar que esses inseridos fora do objeto principal, ñ faz parte dos nomes inseridos usando a template orifinal
 console.log(nagys.hasOwnProperty('firstName')) // true
-console.log(nagys.hasOwnProperty('species')) // false
+console.log(nagys.hasOwnProperty('species')) // false*/
+
+//
