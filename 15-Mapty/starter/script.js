62fccd226436000b9f7d6f9d5e089e630a58855b
henriquenagy <nagy-martins@hotmail.com>
2024-09-17 13:25:37 -0300

    terçou

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -1,8 +1,6 @@
 'use strict'
-
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const form = document.querySelector('.form')
 const containerWorkouts = document.querySelector('.workouts')
 const inputType = document.querySelector('.form__input--type')
@@ -18,8 +16,10 @@ if (navigator.geolocation)
    const { latitude } = position.coords
    const { longitude } = position.coords
    console.log(latitude, longitude) // -24.2876416 -46.9598208
-   console.log(` https://www.google.com/maps/@${latitude},${longitude}`)
+   console.log(`https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`)
    const coords = [latitude, longitude]
+
+   //console.log(map) só para vermos todas as funcionalidades atreladas a essa biblioteca, quais podemos usar e quais são protegidas com _
 
    const map = L.map('map').setView(coords, 13)
 
@@ -27,8 +27,24 @@ if (navigator.geolocation)
     attribution:
      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
    }).addTo(map)
-
-   L.marker(coords).addTo(map).bindPopup('A pretty CSS popup.<br> Easily customizable.').openPopup()
+   //map.on nós que add com base na biblioteca do leaflet, não é algo do JS. o L marker já veio padrão q copiei do site, só inserirmos ai dentro
+   map.on('click', function (mapEvent) {
+    console.log(mapEvent) // Nos mostra todos os eventos que podemos usar quando clica no mapa > {originalEvent: PointerEvent, containerPoint: p, layerPoint: p, latlng: v, type: 'click', …} e outros...
+    const { lat, lng } = mapEvent.latlng
+    L.marker([lat, lng])
+     .addTo(map)
+     .bindPopup(
+      L.popup({
+       maxWidth: 250,
+       minWidth: 200,
+       autoclose: false,
+       colseOnclick: false,
+       className: 'running-popup cycling-popup'
+      })
+     )
+     .setPopupContent('Workout')
+     .openPopup()
+   })
   },
   function () {
    alert('Could not get your position')
