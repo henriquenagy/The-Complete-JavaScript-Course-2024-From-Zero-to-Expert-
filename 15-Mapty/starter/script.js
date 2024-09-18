59674b5e6e4fa7bb1802317ec17711564d500908
henriquenagy <nagy-martins@hotmail.com>
2024-09-18 11:57:37 -0300

    quarta

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -8,45 +8,80 @@ const inputDistance = document.querySelector('.form__input--distance')
 const inputDuration = document.querySelector('.form__input--duration')
 const inputCadence = document.querySelector('.form__input--cadence')
 const inputElevation = document.querySelector('.form__input--elevation')
+let map, mapEvent // Global variable
 
-if (navigator.geolocation)
- navigator.geolocation.getCurrentPosition(
-  function (position) {
-   console.log(position) // GeolocationPosition   coords: GeolocationCoordinates {latitude: -24.2876416, longitude: -46.9598208, altitude: null, accuracy: 3565.3799699389365, altitudeAccuracy: null, …} timestamp :  1726498260417
-   const { latitude } = position.coords
-   const { longitude } = position.coords
-   console.log(latitude, longitude) // -24.2876416 -46.9598208
-   console.log(`https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`)
-   const coords = [latitude, longitude]
+//Implementando o code com base no mapty architecture que o jonas fez
+class App {
+ constructor() {}
 
-   //console.log(map) só para vermos todas as funcionalidades atreladas a essa biblioteca, quais podemos usar e quais são protegidas com _
-
-   const map = L.map('map').setView(coords, 13)
-
-   L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution:
-     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-   }).addTo(map)
-   //map.on nós que add com base na biblioteca do leaflet, não é algo do JS. o L marker já veio padrão q copiei do site, só inserirmos ai dentro
-   map.on('click', function (mapEvent) {
-    console.log(mapEvent) // Nos mostra todos os eventos que podemos usar quando clica no mapa > {originalEvent: PointerEvent, containerPoint: p, layerPoint: p, latlng: v, type: 'click', …} e outros...
-    const { lat, lng } = mapEvent.latlng
-    L.marker([lat, lng])
-     .addTo(map)
-     .bindPopup(
-      L.popup({
-       maxWidth: 250,
-       minWidth: 200,
-       autoclose: false,
-       colseOnclick: false,
-       className: 'running-popup cycling-popup'
-      })
-     )
-     .setPopupContent('Workout')
-     .openPopup()
+ //--------------Método position
+ _getPosition() {
+  if (navigator.geolocation)
+   navigator.geolocation.getCurrentPosition(this._loadMap, function () {
+    alert('Could not get your position')
    })
-  },
-  function () {
-   alert('Could not get your position')
-  }
- )
+ }
+ //--------------Método loadMap
+ _loadMap(position) {
+  console.log(position) // GeolocationPosition
+  const { latitude } = position.coords
+  const { longitude } = position.coords
+  console.log(latitude, longitude) // -24.2876416 -46.9598208
+  console.log(`https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`)
+  const coords = [latitude, longitude]
+
+  map = L.map('map').setView(coords, 13)
+
+  L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+   attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map)
+
+  map.on('click', function (mapE) {
+   mapEvent = mapE
+   form.classList.remove('hidden')
+   inputDistance.focus()
+  })
+ }
+
+ //--------------Método Showform
+ _showForm() {}
+
+ //--------------Método Togglelevation
+ _toggleElevationField() {}
+
+ //--------------Método New workout
+ _newWorkout() {
+  //newRunning(){}
+  //newCycling(){}
+ }
+}
+
+form.addEventListener('submit', function (e) {
+ e.preventDefault()
+
+ // Clear input fields
+ inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = ''
+
+ // Display Marker
+ const { lat, lng } = mapEvent.latlng
+ L.marker([lat, lng])
+  .addTo(map)
+  .bindPopup(
+   L.popup({
+    maxWidth: 250,
+    minWidth: 200,
+    autoclose: false,
+    closeOnclick: false,
+    className: 'running-popup cycling-popup'
+   })
+  )
+  .setPopupContent('Workout')
+  .openPopup()
+})
+
+// Quando trocar o type de running para cycling, ai já troca também o cadence para Elev gain
+inputType.addEventListener('change', function () {
+ inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+ inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
