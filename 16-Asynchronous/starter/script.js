959a426950589f6af74b4dd88c8cc7e4a540bd5a
henriquenagy <nagy-martins@hotmail.com>
2024-10-01 12:14:37 -0300

    terça

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,7 +1,8 @@
 //https://github.com/public-apis/public-apis
 //https://restcountries.com
 'use strict'
-//-----------------------------------------------------------
+
+/* //---------------------------PRIMEIRO MÉTODO
 const countriesContainer = document.querySelector('.countries')
 
 const renderCountry = function (data, classname = '') {
@@ -61,6 +62,50 @@ const getCountryAndNeighbour = function (country) {
   })
  })
 }
-
 // Sample countries whose details we want to display.
-getCountryAndNeighbour('brazil')
+getCountryAndNeighbour('brazil') */
+
+//--------------------------- SEGUNDO MÉTODO AULA 252 PRA FRENTE
+const countriesContainer = document.querySelector('.countries')
+const renderCountry = function (data, classname = '') {
+ const languages = Object.values(data.languages)
+ const currencies = Object.values(data.currencies)
+ const html = `
+ <article class="country ${classname}">
+ <img class="country__img" src= "${data.flags.svg}">
+ <div class="country__data">
+  <h3 class="country__name"> ${data.name.official}</h3>
+  <h4 class="country__region">${data.region}</h4>
+  <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(
+   1
+  )} million</p>         
+  <p class="country__row"><span>🗣️</span>${languages[0]}</p>
+  <p class="country__row"><span>💰</span>${currencies[0].name}</p>
+  </div>
+ </article>`
+
+ countriesContainer.insertAdjacentHTML('beforeend', html)
+ countriesContainer.style.opacity = 1
+}
+
+//-------------------MANEIRA COM O PROMISE 1
+const getCountryData = function (country) {
+ fetch(`https://restcountries.com/v3.1/name/${country}`)
+  .then(function (response) {
+   console.log(response)
+   return response.json()
+  }) //até aqui é promise, abaixo o then é o callback
+  .then(function (data) {
+   console.log(data)
+   renderCountry(data[0])
+  })
+}
+getCountryData('brazil')
+
+//-------------------MANEIRA COM O PROMISE 2 - Mais enxuto
+const getCountryData = function (country) {
+ fetch(`https://restcountries.com/v3.1/name/${country}`)
+  .then(response => response.json())
+  .then(data => renderCountry(data[0]))
+}
+getCountryData('brazil')
